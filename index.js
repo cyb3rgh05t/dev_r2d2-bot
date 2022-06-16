dfa70e8098c370ec4d5759801d3c04e3d62972dc
cyb3rgh05t <flammang.yves@gmail.com>
2022-06-16 20:29:36 +0200

    some updates

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 // StreamNet Club
 
 
-const { Client, Intents, Collection } = require("discord.js");
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
-const fs = require('fs');
+const { Client, Intents, Collection, MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const fs = require('node:fs');
 const path = require('node:path');
 require('dotenv').config({path: path.relative(process.cwd(), path.join(__dirname, 'config','.env'))});
 const config = require('dotenv').config({path: path.relative(process.cwd(), path.join(__dirname, 'config','.env'))});
@@ -85,20 +84,28 @@ client.on('interactionCreate', async interaction => {
 client.config = config;
 client.commands = new Collection();
 
-const events = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-for (const file of events) {
-  const eventName = file.split(".")[0];
-  const event = require(`./events/${file}`);
-  client.on(eventName, event.bind(null, client));
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
-const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-for (const file of commands) {
-  const commandName = file.split(".")[0];
-  const command = require(`./commands/${file}`);
+const commandsPath = path.join(__dirname, 'commandsslashcommands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-  console.log(`Attempting to load command ${commandName}`);
-  client.commands.set(commandName, command);
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
 }
 
 // Bot command prefix.
