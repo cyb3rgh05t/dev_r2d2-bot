49834e474e02ffd39f17679b72f7b2aab586022f
cyb3rgh05t <flammang.yves@gmail.com>
2022-06-16 18:04:35 +0200

    added commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+exports.run = (client, message, args) => {
+    message.channel.send("pong!").catch(console.error);
+}
 
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
-};
+exports.name = "ping";
