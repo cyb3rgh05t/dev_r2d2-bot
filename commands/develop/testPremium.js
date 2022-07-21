ada56f32a54c717db2ac375ca43aa98ec5aff472
cyb3rgh05t <flammang.yves@gmail.com>
2022-07-21 19:35:59 +0200

    typo fix

--- commands/develop/testPremium.js
+++ commands/develop/testPremium.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "testpremium",
     description: "Test the premium command",
     usage: "/testpremium",
-    permisson: "ADMINISTRATOR",
+    permission: "ADMINISTRATOR",
     premium: true,
     execute(interaction) {
 
